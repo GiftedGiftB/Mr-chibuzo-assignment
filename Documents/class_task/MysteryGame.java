1019f9aea2281f09acd416e370fd9e1f38562cbe
GiftedGift <askhamegift@gmail.com>
2025-04-27 13:07:38 -0700

    'classWork'

---
+++ Documents/class_task/MysteryGame.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.Scanner;
+public class MysteryGame{
+	public static void main(String[] args){
+	Random random = new Random();
+	Scanner inputScanner = new Scanner(System.in);
+	
+	int randomGuess = random.nextInt(10) + 1;
+	System.out.println("randomGuess is"+ randomGuess);	
+
+	while(true) {
+	System.out.print("Guess a number: ");
+	int userGuess = inputScanner.nextInt();
+	
+	if(randomGuess == userGuess){
+	System.out.println("you are correct");
+	break;
+	}
+	else if(userGuess < randomGuess){
+	System.out.println("Too high");
+	}
+	else if(userGuess > randomGuess){
+	System.out.println("Too low");
+    }
+  }
+}
+}
