a2e39aafe3696d1eb265dba301a4c62511b1ce66
GiftedGift <askhamegift@gmail.com>
2025-04-29 22:46:16 -0700

    'classWork'

--- Documents/class_task/FuelBudget.java
+++ Documents/class_task/FuelBudget.java
@@ -1,3 +1,8 @@
+//prompt thhe user to enter budget
+//save the user budget with a variable name called user budget
+//initialise price per liter of fuel which is #855 to a variable name called price per liter
+//divide user budget with price per liter.
+
 import java.util.Scanner;
 public class FuelBudget{
 	public static void main(String[] args){
@@ -5,6 +10,7 @@ public class FuelBudget{
 
 	int pricePerLiter = 855;
 	double literOfFuel = 0;
+
 	System.out.print("Enter your budget: ");
 	double UserBudget = scan.nextInt();
 
