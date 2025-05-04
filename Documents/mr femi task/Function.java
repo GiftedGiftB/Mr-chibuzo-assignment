04ba0d886daecc589a4558f2975460beccadea5b
GiftedGift <askhamegift@gmail.com>
2025-05-04 09:45:02 -0800

    'task'

---
+++ Documents/mr femi task/Function.java
@@ -0,0 +1,21 @@
+public class Function{
+/**
+public static int findMultiple(int number){
+	return (number * 5);
+
+} **/
+/**
+public static int computeSumOfNumber(int number){
+
+int number1 = (number / 1000) % 10;
+int number2 = (number / 100) % 10;
+int number3 = (number / 10) % 10;
+int number4 = (number / 1) % 10;
+
+int result = number1 + number2 + number3 + number4;
+	return result;
+} **/
+
+
+}
+
