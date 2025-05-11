9748bdd9783632449d97dfc1122826f1d46a5999
GiftedGift <askhamegift@gmail.com>
2025-05-11 12:59:08 -0800

    'task'

---
+++ Desktop/Jar/AtmTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+public class AtmTest{
+
+	@Test
+
+	public void testCanWithdrawFunds(){
+	
+	  AutomatedTellerMachine atm = new AutomatedTellerMachine();
+
+
+	}
+
+}	
